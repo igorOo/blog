78bd72d7c03a05292b1e65d5e7fb25b70db8faf0
Igor <sidorenko.iv@mail.ru>
2021-02-25 23:08:59 +0300

    rest для angular

--- src/app/pipes/filterFor.pipe.ts
+++ src/app/pipes/filterFor.pipe.ts
@@ -1,19 +1,23 @@
 import {Pipe, PipeTransform} from "@angular/core";
 
 @Pipe({
-    name: 'filterFor'
+    name: 'filterFor',
+    pure: false,
 })
 export default class FilterForPipe implements PipeTransform{
     transform(value: any, start?: number, end?: number): any {
         let result = []
-        if (start != undefined){
-            if (end != undefined){
-                if (value>=start && value<end){
-                    result.push(value)
-                }
-            }else{
-                if (value == start){
-                    result.push(value)
+        if (Object.keys(value).length){
+            if (start != undefined){
+                if (end != undefined){
+                    for (let i=start; i<=end; i++){
+                        result.push(value[i]);
+                    }
+                    if (value>=start && value<end){
+                        result.push(value)
+                    }
+                }else{
+                    result.push(value[start])
                 }
             }
         }

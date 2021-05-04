a0d89a7a1d3a59bc04c31d7cb1d35528946eee02
igor <mail@mail.ru>
2021-05-05 00:01:02 +0300

    Категории

--- src/app/pages/category/news/news.component.ts
+++ src/app/pages/category/news/news.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {News} from "../../../models/News";
+import {environment} from "../../../../environments/environment";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+    selector: 'app-news',
+    templateUrl: './news.component.html',
+    styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+    public list!: Array<News>
+    public loading: boolean = true
+    public page: number = 1
 
-  ngOnInit(): void {
-  }
+    constructor(private  http: HttpClient, private router: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.http.get(environment.restUrl + "/api/v1/news/category/" + this.router.snapshot.params["translit"]+"?page="+this.page,
+            {
+                headers: {'Content-Type': 'application/json'}
+            })
+            .subscribe(result => {
+                console.log(result)
+            })
+    }
 
 }

40d5062baa0ffdd54be19837ee19d0b363d33f48
igor <sidorenko.iv@mail.ru>
2021-09-23 20:50:53 +0300

    for checkout

--- src/app/pages/add-post/add-post.module.ts
+++ src/app/pages/add-post/add-post.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {SharedModule} from "../../shared/modules/shared-module/shared.module";
 import {AddPostRoutingModule} from "./add-post-routing.module";
 import {IndexComponent} from "./components/index/index.component";
+import {EditorModule} from "@tinymce/tinymce-angular";
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import {IndexComponent} from "./components/index/index.component";
     imports: [
         CommonModule,
         SharedModule,
-        AddPostRoutingModule
+        AddPostRoutingModule,
+        EditorModule,
     ],
     exports: [IndexComponent]
 })

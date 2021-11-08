79692f55a68e7fc10f41b67a8a0df6cb41d63344
user <sidorenko.iv@mail.ru>
2021-11-08 22:58:37 +0300

    Остальное не закомиченное

--- src/app/pages/add-post/add-post.module.ts
+++ src/app/pages/add-post/add-post.module.ts
@@ -4,17 +4,25 @@ import {SharedModule} from "../../shared/modules/shared-module/shared.module";
 import {AddPostRoutingModule} from "./add-post-routing.module";
 import {IndexComponent} from "./components/index/index.component";
 import {EditorModule} from "@tinymce/tinymce-angular";
-
+import {TagsInputComponent} from "../../shared/components/tags-input/tags-input.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CropperComponent} from "../../shared/components/cropper/cropper.component";
+import {ImageCropperModule} from "ngx-image-cropper";
 
 @NgModule({
     declarations: [
-        IndexComponent
+        IndexComponent,
+        TagsInputComponent,
+        CropperComponent
     ],
     imports: [
         CommonModule,
         SharedModule,
         AddPostRoutingModule,
         EditorModule,
+        ReactiveFormsModule,
+        ImageCropperModule,
+        FormsModule,
     ],
     exports: [IndexComponent]
 })
